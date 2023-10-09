41dddd774e9f57b000232feb98a4ad2acf3819f1
neosInPocket <88581498+neosInPocket@users.noreply.github.com>
2023-10-09 10:20:18 +0300

    changes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private CircleCollider2D mainCircle;
+	[SerializeField] private CircleCollider2D playerCollider;
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private float rotationSpeed;
+	[SerializeField] private float launchSpeed;
+	private bool isCentered;
+	private CircleCollider2D currentCircle;
+	
+	private void Start()
+	{
+		EnhancedTouchSupport.Enable();
+		TouchSimulation.Enable();
+		Touch.onFingerDown += LaunchPlayer;
+		
+		SetPlayerInPosition();
+	}
+	
+	private void Update()
+	{
+		if (!isCentered) return;
+		
+		transform.RotateAround(currentCircle.transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+	}
+	
+	private void LaunchPlayer(Finger finger)
+	{
+		rb.gravityScale = 1;
+		rb.AddForce(-transform.up * launchSpeed, ForceMode2D.Impulse);
+	}
+	
+	private void SetPlayerInPosition()
+	{
+		currentCircle = mainCircle;
+		var positionX = mainCircle.transform.position.x - mainCircle.radius;
+		var positionY = mainCircle.transform.position.y;
+		transform.position = new Vector2(positionX, positionY);
+		isCentered = true;
+	}
+	
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.TryGetComponent<CircleCollider2D>(out CircleCollider2D circle))
+		{
+			if (circle == currentCircle) return;
+			
+			currentCircle = circle;
+		}
+	}
+	
+	private void OnDestroy()
+	{
+		EnhancedTouchSupport.Disable();
+		TouchSimulation.Disable();
+	}
+}
